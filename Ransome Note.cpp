1c6667db966bd62a6b90252ac4e2351b9f465187
tonyache <tonyache@gmail.com>
2018-03-24 21:36:43 -0400

    Add files via upload

---
+++ Ransome Note.cpp
@@ -0,0 +1,52 @@
+//
+//  Ransome Note.cpp
+//  Leetcode
+//
+//  Created by Antonio Ache on 3/24/18.
+//  Copyright © 2018 Antonio Ache. All rights reserved.
+//
+
+#include "Ransome Note.hpp"
+#include<iostream>
+#include<stdlib.h>
+#include<vector>
+#include<unordered_map>
+#include<queue>
+
+using namespace std;
+
+
+bool canConstruct(string ransomNote, string magazine) {
+    bool answer=true;
+    std::unordered_map<char,int> table;
+    char c;
+    int freq;
+    for(std::string::iterator it=magazine.begin();it!=magazine.end();it++){
+        c=*it;
+        if(table.count(c)==0){
+            table.insert(std::pair<char,int>(c,1));
+        }
+        else{
+            freq=table.find(c)->second;
+            table.find(c)->second=freq+1;
+        }
+    }
+    for(std::string::iterator it=ransomNote.begin();it!=ransomNote.end();it++){
+        char c=*it;
+        if(table.count(c)>0){
+            freq=table.find(c)->second;
+            if(freq>1){
+                table.find(c)->second=freq-1;
+            }
+            else{
+                table.erase(table.find(c));
+            }
+        }
+        else{
+            answer=false;
+            break;
+        }
+        
+    }
+    return answer;
+}
