91537ba63ef06952b89b6761f1f61936c8c9714d
tonyache <tonyache@gmail.com>
2018-04-27 17:11:27 -0400

    Update image smoother.cpp

--- image smoother.cpp
+++ image smoother.cpp
@@ -6,6 +6,11 @@
 //  Copyright © 2018 Antonio Ache. All rights reserved.
 //
 
+// Given a 2D integer matrix M representing the gray scale of an image, you need to design a smoother to make the gray scale 
+// of each cell becomes the average gray scale (rounding down) of all the 8 surrounding cells and itself. 
+// If a cell has less than 8 surrounding cells, then use as many as you can.
+
+
 #include "image smoother.hpp"
 #include<iostream>
 #include<stdlib.h>
